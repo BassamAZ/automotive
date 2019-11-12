6c472e090e5fb8079f64ab4fa432897ed7f2e51a
mohamed ibrahim <mohamed.m.ibrahim84@gmail.com>
2019-11-12 17:34:00 +0400

    dockerizing the solution

---
+++ automotive-gateway/src/main/java/com/automotive/gateway/AutomotiveGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.automotive.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveGatewayApplication.class, args);
+	}
+
+}
